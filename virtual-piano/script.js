16ff35b3d2b9926f3397e60f261eaf2d5c368a79
Gunel Huseynova <gorellik246@gmail.com>
2021-03-21 11:07:00 +0300

    feat: add stopPlayVideo func and toggleFullscreen

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,17 +1,28 @@
 (function () {
 	const piano = document.querySelector('.js-piano');
 	const pianoKeys = document.querySelectorAll('.js-piano-key');
+	const switches = document.querySelectorAll('.js-btn');
+	const fullScreenBtn = document.querySelector('.js-fullscreen');
+
+	let isMouseDown = false;
 
 	function playAudio(event) {
-		const el = event.target;
+		let el;
 
-		if (!el.classList.contains('piano-key')) return;
+		if (event.type === 'keydown') {
+			if (event.repeat) return;
+			el = document.querySelector(`[data-letter='${event.code.slice(-1)}']`);
+		} else {
+			el = event.target;
+		}
+
+		if (!el || !el.classList.contains('piano-key')) return;
 
 		const curKey = el;
-		const curKeyNote = el.dataset.note;
+		const curKeyNote = curKey.dataset.note;
 
 		if (!curKey.classList.contains('piano-key-active')) {
-			curKey.classList.add('piano-key-active');
+			curKey.classList.add('piano-key-active', 'piano-key-active-pseudo');
 		}
 
 		const audio = new Audio();
@@ -21,13 +32,81 @@
 		audio.play();
 	}
 
-	piano.addEventListener('click', playAudio);
+	function stopAudio(event) {
+		let el;
 
-	pianoKeys.forEach((pianoKey) =>
-		pianoKey.addEventListener('transitionend', function (event) {
-			if (event.propertyName === 'transform') {
-				this.classList.remove('piano-key-active');
+		if (event.type === 'keyup') {
+			el = document.querySelector(`[data-letter='${event.code.slice(-1)}']`);
+		} else {
+			el = event.target;
+		}
+
+		if (!el || !el.classList.contains('piano-key')) return;
+
+		el.classList.remove('piano-key-active', 'piano-key-active-pseudo');
+	}
+
+	function startPlayPiano(event) {
+		playAudio(event);
+		isMouseDown = true;
+		pianoKeys.forEach((pianoKey) => {
+			pianoKey.addEventListener('mouseover', playAudio);
+			pianoKey.addEventListener('mouseout', stopAudio);
+		});
+	}
+
+	function stopPlayPiano(event) {
+		if (isMouseDown) {
+			event.target.classList.remove(
+				'piano-key-active',
+				'piano-key-active-pseudo'
+			);
+			isMouseDown = false;
+		}
+
+		pianoKeys.forEach((pianoKey) => {
+			pianoKey.removeEventListener('mouseover', playAudio);
+			pianoKey.removeEventListener('mouseout', stopAudio);
+		});
+	}
+
+	function handleSwitch(event) {
+		const el = event.target;
+
+		switches.forEach((arrEl) => arrEl.classList.remove('btn-active'));
+		el.classList.add('btn-active');
+
+		if (el.classList.contains('btn-letters')) {
+			pianoKeys.forEach((pianoKey) =>
+				pianoKey.classList.add('piano-key-letter')
+			);
+		} else {
+			pianoKeys.forEach((pianoKey) =>
+				pianoKey.classList.remove('piano-key-letter')
+			);
+		}
+	}
+
+	function toggleFullScreen() {
+		if (!document.fullscreenElement) {
+			document.documentElement.requestFullscreen();
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
 			}
-		})
-	);
+		}
+	}
+
+	switches.forEach((switchItem) => {
+		switchItem.addEventListener('click', handleSwitch);
+	});
+
+	fullScreenBtn.addEventListener('click', toggleFullScreen);
+
+	piano.addEventListener('mousedown', startPlayPiano);
+	piano.addEventListener('mouseup', stopPlayPiano);
+	piano.addEventListener('mouseleave', stopPlayPiano);
+
+	window.addEventListener('keydown', playAudio);
+	window.addEventListener('keyup', stopAudio);
 })();
