adcce258e31b285b97885ce87d99d2513503c5fb
Gunel Huseynova <gorellik246@gmail.com>
2021-05-06 17:04:10 +0300

    feat: add accerdion funcionality

--- online-zoo/pages/zoo/zoo.js
+++ online-zoo/pages/zoo/zoo.js
@@ -13,3 +13,31 @@
     });
   });
 })();
+
+(function () {
+  const accerdion = document.querySelector('.js-vacancy-accerdion');
+
+  if (accerdion)
+    accerdion.addEventListener('click', (e) => {
+      const accHead = e.target.closest('.i-accerdion-item__head');
+
+      if (!accHead) return;
+
+      const accParent = accHead.parentElement;
+      const accBody = accParent.querySelector('.i-accerdion-item__body');
+
+      if (accParent.classList.contains('i-accerdion-item--is-open')) {
+        accParent.classList.remove('i-accerdion-item--is-open');
+        accBody.style.maxHeight = '0px';
+      } else {
+        document.querySelectorAll('.i-accerdion-item').forEach((vcard) => {
+          vcard.classList.remove('i-accerdion-item--is-open');
+          vcard.querySelector('.i-accerdion-item__body').style.maxHeight =
+            '0px';
+        });
+
+        accParent.classList.add('i-accerdion-item--is-open');
+        accBody.style.maxHeight = accBody.scrollHeight + 'px';
+      }
+    });
+})();
