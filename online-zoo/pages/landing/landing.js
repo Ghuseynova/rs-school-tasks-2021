84b9cc8455d9a8ede1b01beb6f6909e84ef4f1e2
Gunel Huseynova <gorellik246@gmail.com>
2021-05-06 11:46:49 +0300

    feat: add tslider func

--- online-zoo/pages/landing/landing.js
+++ online-zoo/pages/landing/landing.js
@@ -1,7 +1,5 @@
 (function () {
-  const fSlider = document.querySelector('.f-slider');
   const fSliderContainer = document.querySelector('.f-slider__row');
-  const fSlides = document.querySelectorAll('.f-slider__col');
   const prevBtn = document.querySelector('.js-fslider-prev');
   const nextBtn = document.querySelector('.js-fslider-next');
 
@@ -34,6 +32,70 @@
       setTimeout(() => {
         fSlide.classList.add('fade');
       }, 1);
+    });
+  });
+})();
+
+(function () {
+  const tSliderContainer = document.querySelector('.t-slider__row');
+  const tSlides = document.querySelectorAll('.t-slider__col');
+  const prevBtn = document.querySelector('.js-tslider-prev');
+  const nextBtn = document.querySelector('.js-tslider-next');
+  let timer = 10000;
+
+  function drawSlide(dir) {
+    const tSlides = document.querySelectorAll('.t-slider__col');
+    const cln =
+      dir === 'right'
+        ? tSlides[0].cloneNode(true)
+        : tSlides[tSlides.length - 1].cloneNode(true);
+
+    dir === 'right'
+      ? tSlides[0].remove()
+      : tSlides[tSlides.length - 1].remove();
+
+    dir === 'right'
+      ? tSliderContainer.append(cln)
+      : tSliderContainer.prepend(cln);
+
+    tSlides.forEach((tSlide) => {
+      tSlide.classList.remove('fade');
+
+      setTimeout(() => {
+        tSlide.classList.add('fade');
+      }, 500);
+    });
+  }
+
+  let interval = setInterval(() => {
+    drawSlide('right');
+  }, timer);
+
+  prevBtn.addEventListener('click', () => {
+    drawSlide('left');
+
+    stopAndReInitSlider();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    drawSlide('right');
+
+    stopAndReInitSlider();
+  });
+
+  function stopAndReInitSlider() {
+    clearInterval(interval);
+
+    setTimeout(function () {
+      setInterval(() => {
+        drawSlide('right');
+      }, timer);
+    }, 20000);
+  }
+
+  tSlides.forEach((tSlide) => {
+    tSlide.addEventListener('click', function () {
+      stopAndReInitSlider();
     });
   });
 })();
